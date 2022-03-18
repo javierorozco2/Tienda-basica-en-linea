4d980b6f9f242c71bc9e861424d06bf0e6474645
javierorozco2 <eorozco8@ucol.mx>
2022-03-18 10:45:45 -0600

    v1.0

---
+++ welcome.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenida</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+    <div id="main">
+
+        <div id="main-header">
+            <div>
+               <h2>Tienda basica en linea</h2>
+                <p>&nbspBienvenido</p> 
+            </div>
+            <img src="https://www.picng.com/upload/shopping_cart/png_shopping_cart_33838.png">
+        </div>
+
+        <div id="main-body">
+            <h2>Porfavor, ingresa tu nombre:</h2>
+            <input type="text">
+            <button type="submit">Enviar</button>
+        </div>
+
+    </div>
+    
+</body>
+</html>
